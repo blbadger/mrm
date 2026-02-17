c9b95703babe93fc180a00fc36e22c1b400b6714
Benjamin Badger <blbadger10@gmail.com>
2026-02-17 16:02:04 -0500

    more tests

--- mrm/tests.py
+++ mrm/tests.py
@@ -314,6 +314,80 @@ def test_parallel_mixed_row_col_decay_equivalence():
     print (f"Reference output: {tokenizer.decode(output_ids[0])} \n Cached Output: {tokenizer.decode(cached_output_ids[0])}")
     return
 
+def test_recurrent_row_repeat_equivalence(trained_model=True):
+    load_dotenv()
+    checkpoint_root = os.getenv('CHECKPOINT_ROOT')
+    data_root = os.getenv('DATA_ROOT')
+    tokenizer = AutoTokenizer.from_pretrained(f"{data_root}/tokenizer_fineweb_8k")
+    tokenizer.pad_token = tokenizer.eos_token
+    n_vocab = tokenizer.vocab_size
+    print("Vocab size: ", n_vocab)
+
+    tokenized_length = 512
+    dim = 512
+    layers = 16
+    n_heads = None
+    kernel = 1
+
+    cached_model = CachedInferenceMLPMixer(
+        n_vocab, dim, tokenized_length, layers, heads=n_heads, kernel=kernel, expanded_convs=False, copy=False, 
+        mixed_heads=False, combined_heads=False, decay=True, parallel_heads=False, use_projections=True, dropout_layer=True).float().to(device)
+
+    model = InferenceMLPMixer(
+        n_vocab, dim, tokenized_length, layers, heads=n_heads, kernel=kernel, expanded_convs=False, copy=False, 
+        mixed_heads=False, combined_heads=False, decay=True, parallel_heads=False, use_projections=True, dropout_layer=True).float().to(device)
+
+    generation_config = GenerationConfig()
+    print (model)
+    load_model(model, f"{checkpoint_root}/fineweb_hNone_repeat_k1_512_n16_c512_b32x4/checkpoint-200000/model.safetensors")
+    load_model(cached_model, f"{checkpoint_root}/fineweb_hNone_repeat_k1_512_n16_c512_b32x4/checkpoint-200000/model.safetensors")
+    text ='''Four score and seven years ago, our'''
+    input_ids = torch.tensor(tokenizer.encode(text)[1:]).unsqueeze(0).to(device) # ignore bos token
+    print (input_ids)
+
+    output_ids = model.generate(input_ids, max_length=len(input_ids[0]) + 50, generation_config=generation_config)
+    cached_output_ids = cached_model.generate(input_ids, max_length=len(input_ids[0]) + 50, generation_config=generation_config)
+    print (f"Reference output: {tokenizer.decode(output_ids[0])} \n Cached Output: {tokenizer.decode(cached_output_ids[0])}")
+    assert torch.equal(output_ids, cached_output_ids)
+    return
+
+def test_recurrent_col_repeat_equivalence(trained_model=True):
+    load_dotenv()
+    checkpoint_root = os.getenv('CHECKPOINT_ROOT')
+    data_root = os.getenv('DATA_ROOT')
+    tokenizer = AutoTokenizer.from_pretrained(f"{data_root}/tokenizer_fineweb_8k")
+    tokenizer.pad_token = tokenizer.eos_token
+    n_vocab = tokenizer.vocab_size
+    print("Vocab size: ", n_vocab)
+
+    tokenized_length = 512
+    dim = 1024
+    layers = 16
+    n_heads = 4
+    kernel = 1
+
+    cached_model = RecurrentInference(
+        n_vocab, dim, tokenized_length, layers, heads=n_heads, kernel=kernel, expanded_convs=False, copy=False, 
+        mixed_heads=False, combined_heads=False, decay=False, parallel_heads=False, use_projections=True, dropout_layer=True).float().to(device)
+
+    model = InferenceMLPMixer(
+        n_vocab, dim, tokenized_length, layers, heads=n_heads, kernel=kernel, expanded_convs=False, copy=False, 
+        mixed_heads=False, combined_heads=False, decay=False, parallel_heads=False, use_projections=True, dropout_layer=True).float().to(device)
+
+    generation_config = GenerationConfig()
+    print (model)
+    load_model(model, f"{checkpoint_root}/fineweb_h4_colrepeat_k1_1024_n16_c512_b32x4/checkpoint-200000/model.safetensors")
+    load_model(cached_model, f"{checkpoint_root}/fineweb_h4_colrepeat_k1_1024_n16_c512_b32x4/checkpoint-200000/model.safetensors")
+    text ='''Four score and seven years ago, our'''
+    input_ids = torch.tensor(tokenizer.encode(text)[1:]).unsqueeze(0).to(device) # ignore bos token
+    print (input_ids)
+
+    output_ids = model.generate(input_ids, max_length=len(input_ids[0]) + 50, generation_config=generation_config)
+    cached_output_ids = cached_model.generate(input_ids, max_length=len(input_ids[0]) + 50, generation_config=generation_config)
+    print (f"Reference output: {tokenizer.decode(output_ids[0])} \n Cached Output: {tokenizer.decode(cached_output_ids[0])}")
+    assert torch.equal(output_ids, cached_output_ids)
+    return
+
 def test_recurrent_mixed_row_col_decay_equivalence(trained_model=False):
     load_dotenv()
     checkpoint_root = os.getenv('CHECKPOINT_ROOT')
