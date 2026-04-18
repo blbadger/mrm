f244fa02ceb9a2ce7380a14501643223ceb7d3cb
Benjamin Badger <blbadger10@gmail.com>
2026-04-18 16:36:47 -0400

    dataset

--- mrm/gsq_dataset.py
+++ mrm/gsq_dataset.py
@@ -15,20 +15,29 @@ model_name = "Qwen2.5-Math-7B-Instruct"
 llm = LLM(model_name)
 save_every = 1000
 
-for i in range(len(train_dataset)):
-    text=f'''<|im_start|>system\nPlease give correct answers with questions, matching the exact format of the examples given.<|im_end|>
-<|im_start|>user\nGiven the following examples, write a new grade-school mathematical Question and Answer in the same format:\n{train_dataset['prompt'][i]}<|im_end|>
-<|im_start|>assistant\n'''
-    output = llm.generate(
-        text, 
-        sampling_params=SamplingParams(
-            n=100, temperature=0.7, 
-            top_p=0.9, 
-            max_tokens=2048,
-            stop=["</s>", "<|im_end|>", "<|endoftext|>"],
-            stop_token_ids=[151645, 151643],
-            tensor_parallel_size=4)
-        )
-    all_outputs.append(output)
+output_path = '/home/badger/gsq_dataset'
 
-    print(output[0].outputs[0].text)
+if __name__ == '__main__':
+    for i in range(len(train_dataset)):
+        text=f'''<|im_start|>system\nPlease give correct answers with questions, matching the exact format of the examples given.<|im_end|>
+    <|im_start|>user\nGiven the following examples, write a new grade-school mathematical Question and Answer in the same format:\n{train_dataset['prompt'][i]}<|im_end|>
+    <|im_start|>assistant\n'''
+        output = llm.generate(
+            text, 
+            sampling_params=SamplingParams(
+                n=100, temperature=0.7, 
+                top_p=0.9, 
+                max_tokens=2048,
+                stop=["</s>", "<|im_end|>", "<|endoftext|>"],
+                stop_token_ids=[151645, 151643],
+                tensor_parallel_size=4)
+            )
+        all_outputs.append(output)
+        if i % save_every == 0 and i > 0:
+                dataset_dict = {'samples': all_outputs}
+                dataset = Dataset.from_dict(dataset_dict)
+                dataset.save_to_disk(output_path + f'/{process_index}_{step}')
+                total_tokens = []
+                total_values= []
+
+        print(output[0].outputs[0].text)
