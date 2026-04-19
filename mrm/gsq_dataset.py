ea3d82e97cac57595721f76b6eaf9b32cee8c968
blbadger <blbadger10@gmail.com>
2026-04-19 18:27:11 +0000

    debugged gsq

--- mrm/gsq_dataset.py
+++ mrm/gsq_dataset.py
@@ -1,8 +1,11 @@
 from vllm import LLM, SamplingParams
 from datasets import load_dataset, load_from_disk, Dataset
+from tqdm import tqdm
+import random
 
-def prepare_nshot(example, n_shot=3):
-    three_shot_prompt = '\n'.join([f"Question: {train_dataset[i]['question']} \nAnswer: {train_dataset[i]['answer']}" for i in range(n_shot)])
+def prepare_nshot(example, n_shot=5):
+    start_index = random.randint(0, len(train_dataset) -n_shot)
+    three_shot_prompt = '\n'.join([f"Question: {train_dataset[i+start_index]['question']} \nAnswer: {train_dataset[i+start_index]['answer']}" for i in range(n_shot)])
     example['prompt'] = f"{three_shot_prompt}"
     return example
 
@@ -10,34 +13,37 @@ dataset = load_dataset("openai/gsm8k", "main")
 train_dataset = dataset['train']
 train_dataset = train_dataset.map(prepare_nshot, num_proc=16)
 
-## VLLM output
-model_name = "Qwen2.5-Math-7B-Instruct"
-llm = LLM(model_name)
-save_every = 1000
-
-output_path = '/home/badger/gsq_dataset'
-
 if __name__ == '__main__':
-    for i in range(len(train_dataset)):
-        text=f'''<|im_start|>system\nPlease give correct answers with questions, matching the exact format of the examples given.<|im_end|>
-    <|im_start|>user\nGiven the following examples, write a new grade-school mathematical Question and Answer in the same format:\n{train_dataset['prompt'][i]}<|im_end|>
-    <|im_start|>assistant\n'''
+    ## VLLM output
+    model_name = "Qwen/Qwen2.5-Math-7B"
+    llm = LLM(model_name, tensor_parallel_size=4)
+    save_every = 200
+    output_path = '/home/badger/gsq_dataset'
+
+    all_outputs = []
+    for i in tqdm(range(len(train_dataset))):
+        text=f'''<|im_start|>system\nPlease write a single question with a correct answer, matching the exact format of the examples given (answer follows ####, computation in <<>>).<|im_end|>
+    <|im_start|>user\nGiven the following examples, write a new grade-school mathematical question distinct from the ones provided and provide a correct answer in the same format as shown in the examples given. Do not simply answer existing questions, write new a one and answer that. Do not box the answer, use the format provided. Examples: \n\n{train_dataset['prompt'][i]}<|im_end|>
+    <|im_start|>assistant\n Question: '''
+        text = f"{train_dataset['prompt'][i]}\nQuestion:"
+        #print (train_dataset['prompt'][i])
         output = llm.generate(
             text, 
+            use_tqdm=False,
             sampling_params=SamplingParams(
-                n=100, temperature=0.7, 
-                top_p=0.9, 
-                max_tokens=2048,
+                n=200, temperature=0.7, 
+                top_p=0.9,
+                max_tokens=512,
                 stop=["</s>", "<|im_end|>", "<|endoftext|>"],
                 stop_token_ids=[151645, 151643],
-                tensor_parallel_size=4)
+                )
             )
-        all_outputs.append(output)
+        all_outputs += [output[0].outputs[i].text for i in range(len(output))]
         if i % save_every == 0 and i > 0:
                 dataset_dict = {'samples': all_outputs}
                 dataset = Dataset.from_dict(dataset_dict)
-                dataset.save_to_disk(output_path + f'/{process_index}_{step}')
-                total_tokens = []
-                total_values= []
+                dataset.save_to_disk(output_path + f'/_{i}')
+                all_outputs = []
 
-        print(output[0].outputs[0].text)
+                print(output[0].outputs[0].text)
+                print (output[0].outputs[1].text)
