8d230a24b38d1025f0d847b4a3d5a1d46a7cbf81
blbadger <blbadger10@gmail.com>
2026-04-20 17:14:19 +0000

    wrapped up clean

--- mrm/balanced_grpo_trainer.py
+++ mrm/balanced_grpo_trainer.py
@@ -2,7 +2,7 @@ import torch
 import re
 from trl import GRPOConfig, GRPOTrainer
 from safetensors.torch import load_model
-from transformers import AutoTokenizer, LlamaConfig, LlamaForCausalLM
+from transformers import AutoTokenizer, LlamaConfig
 from datasets import load_dataset, load_from_disk, Dataset
 from dual_srm import DualMLPMixer
 from dotenv import load_dotenv
@@ -12,6 +12,7 @@ from transformers.modeling_outputs import CausalLMOutput
 import shutil
 
 # This trainer expects trl==0.15.3 with trl/trainer/grpo_trainer.py overwritten with the sampled trained available here: https://github.com/blbadger/trl/blob/main/trl/trainer/sampled_grpo_trainer.py. The balanced train batch size and sampling top_p are supplied in that file (bad practice I know)
+
 
 class DualMixer(DualMLPMixer, GenerationMixin):
 
@@ -53,7 +54,6 @@ class DualMixer(DualMLPMixer, GenerationMixin):
         self.cache_built = False
         self.device = self.output_layer.weight.device
         self.warnings_issued={}
-        self.index = 0
     
     def add_model_tags(self, tag):
         print (tag)
@@ -69,9 +69,6 @@ class DualMixer(DualMLPMixer, GenerationMixin):
 
     def _is_stateful(self):
         return False
-    
-    def is_remote_code(self):
-        return True
 
     def build_cache(self, input_ids):
         for i in range(len(input_ids[0])-1):
@@ -79,7 +76,6 @@ class DualMixer(DualMLPMixer, GenerationMixin):
             for block in self.mixer_blocks:
                 x = block(x, i, True)
         self.cache_built = True
-        self.index = i+1
         return
 
     def clear_cache(self):
@@ -87,13 +83,12 @@ class DualMixer(DualMLPMixer, GenerationMixin):
             for h in range(len(block.token_mixing_layer.mixer_heads)):
                 block.token_mixing_layer.mixer_heads[h].cache = torch.zeros(self.hidden_dim//self.n_heads).to('cuda') # only for mixed heads
         self.cache_built = False
-        self.index = 0
 
     def forward(self, input_ids, labels=None, **kwargs):
         is_recurrent = input_ids.shape[1] < self.seq_len
         if not self.cache_built and is_recurrent:
             self.build_cache(input_ids)
-        index = self.index
+        index = input_ids.shape[1] - 1
         if is_recurrent:
             input_ids = input_ids[:, -1] # last token only
         # model's forward pass
@@ -101,14 +96,12 @@ class DualMixer(DualMLPMixer, GenerationMixin):
         for block in self.mixer_blocks:
             x = block(x, index, is_recurrent)
         logits = self.output_layer(x).unsqueeze(1)
-        self.index += 1
         if labels is not None:
             shift_logits = logits[:, :-1].contiguous()
             shift_labels = labels[:, 1:].contiguous()
             shift_logits = shift_logits.view(-1, self.vocab_size)
             shift_labels = shift_labels.view(-1)
             loss = self.loss_fn(shift_logits, shift_labels)
-
             return CausalLMOutput(loss=loss, logits=logits)
         else:
             return CausalLMOutput(loss=0, logits=logits)
@@ -135,11 +128,9 @@ def output_extract(predicted_output):
 def correctness_reward_func(prompts, completions, answer, **kwargs) -> list[float]:
     extracted_responses = [output_extract(c) for c in completions]
     extracted_answers = [output_extract(a) for a in answer]
-    #print (completions[0], answer[0], extracted_responses[0], extracted_answers[0])
     rewards = [1.0 if r == a else 0.0 for r, a in zip(extracted_responses, extracted_answers)]
 
-#    print('='*60, f"Question:\n{prompts[index]}", f"\nAnswer:\n{answer[index]}\n",'-'*50, f"\nResponse:\n{completions[index]}", f"\nExtracted:\n{extracted_responses[index]}\n")
-    return [1.0 if r == a else 0.0 for r, a in zip(extracted_responses, extracted_answers)]
+    return rewards 
 
 def format_reward_func(completions, **kwargs) -> list[float]:
     """Reward function that checks if the completion has a specific format."""
@@ -168,7 +159,7 @@ if __name__ == '__main__':
     tokenizer.pad_token = tokenizer.eos_token
     n_vocab = len(tokenizer)
     print("Vocab size: ", n_vocab)
-	
+
     tokenized_length = 1024
     dim = 1024
     layers = 16
@@ -181,7 +172,6 @@ if __name__ == '__main__':
     model.is_gradient_checkpointing = False
     print (model)
 
-    #model = LlamaForCausalLM.from_pretrained(f'{checkpoint_root}/gsm8k_SFT_transformer_c1024/checkpoint-300')
     dataset = load_dataset("openai/gsm8k", "main")
     train_dataset, eval_dataset = dataset['train'], dataset['test']
     print (train_dataset[0])
@@ -189,16 +179,13 @@ if __name__ == '__main__':
     print (train_dataset[0])
     eval_dataset = eval_dataset.map(prepare_nshot, num_proc=16)
     print (len(train_dataset))
-    model_path=f'{checkpoint_root}/fineweb_h4_decay_nonparallel_mixed_projs_k1_1024_n16_c1024_b16x4/checkpoint-200000/model.safetensors'
-    #model_path=f'{checkpoint_root}/gsm8k_SFT_srm_c1024/meta-chkpt-300/model.safetensors'
+    #model_path=f'{checkpoint_root}/fineweb_h4_decay_nonparallel_mixed_projs_k1_1024_n16_c1024_b16x4/checkpoint-200000/model.safetensors'
+    model_path = f'{checkpoint_root}/gsm8k_SFT_srm_c1024/chkpt-300/model.safetensors'
     load_model(model, model_path)
-    model = model.to('cuda')
-    input_ids = tokenizer.encode('Q: What is two plus two? A: Four. Q: What is one plus four? A:', return_tensors='pt', add_special_tokens=False).to('cuda')
-    output = torch.tensor(model.generate(input_ids, max_new_tokens=16, temperature=0., do_sample=False))
-    print ('\n\n', output, tokenizer.decode(output[0]), '\n\n')
+
     max_prompt_length = tokenized_length - 256
 
-    output_dir = f'{checkpoint_root}/gsm8k_srm_s5_b15x4'
+    output_dir = f'{checkpoint_root}/gsm8k_srm_grpo_s50_b16x4'
     training_args = GRPOConfig(
         learning_rate = 2e-5,
         weight_decay = 0.1,
@@ -206,10 +193,9 @@ if __name__ == '__main__':
         lr_scheduler_type = "cosine",
         optim = "adamw_torch",
         logging_steps = 1,
-        per_device_train_batch_size=15,
-        steps_per_generation=1,
+        per_device_train_batch_size=100,
         gradient_accumulation_steps=1,
-        num_generations = 5, 
+        num_generations = 50, 
         max_completion_length = tokenized_length - max_prompt_length,
         num_train_epochs = 3,
         save_steps = 100,
@@ -217,9 +203,8 @@ if __name__ == '__main__':
         report_to = "none",
         output_dir = output_dir,
         fp16=True,
-        beta=0.,
         torch_compile=True, 
-        temperature = 0.7, 
+        temperature = 0.7, # NB: top_p=0.9 supplied directly to generate in grpo_trainer
 )
 	
     trainer = GRPOTrainer(
@@ -238,4 +223,4 @@ if __name__ == '__main__':
         os.mkdir(output_dir) 
     shutil.copy(code_path, output_dir) 
     #training_args.save_json(output_dir + '/checkpoint-1250')
-    trainer.train(output_dir + '/checkpoint-400')
+    trainer.train()
